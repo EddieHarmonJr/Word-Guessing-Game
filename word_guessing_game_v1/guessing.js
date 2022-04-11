ed3ce69777aff723c6c2e5cd39c903a908bb8f3b
EddieHarmonJr <eddieharmon@ac.c4q.nyc>
2022-04-11 16:17:16 -0400

    correct javascript file in html document and create variable that holds reference to the button with class btn__reset

--- word_guessing_game_v1/guessing.js
+++ word_guessing_game_v1/guessing.js
@@ -1,3 +1,5 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const missed = 0;
+
+const startButton = document.getElementsByClassName('btn__reset')[0];
