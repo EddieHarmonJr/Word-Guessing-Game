249368db4b4b645e305417b61f4b7ddd064137ef
EddieHarmonJr <eddieharmon@ac.c4q.nyc>
2022-04-11 16:25:54 -0400

    add an event listener to the start game button to hide the start screen overlay

--- word_guessing_game_v1/guessing.js
+++ word_guessing_game_v1/guessing.js
@@ -2,4 +2,9 @@ const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const missed = 0;
 
+const overlay = document.getElementById('overlay');
+
 const startButton = document.getElementsByClassName('btn__reset')[0];
+startButton.addEventListener('click', (e) => {
+  overlay.style.display = 'none';
+});
