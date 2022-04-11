08e74f8c11d2093039b08f3dcd7d346563665c85
EddieHarmonJr <eddieharmon@ac.c4q.nyc>
2022-04-11 16:32:53 -0400

    create a phrases array that contains at least 5 different phrases as strings

--- word_guessing_game_v1/guessing.js
+++ word_guessing_game_v1/guessing.js
@@ -4,6 +4,14 @@ const missed = 0;
 
 const overlay = document.getElementById('overlay');
 
+const phrases = [
+  "DID I DO THAT",
+  "HASTA LA VISTA BABY",
+  "YOU HAD ME AT HELLO",
+  "BETTER LATE THAN NEVER",
+  "AT THE END OF THE DAY"
+];
+
 const startButton = document.getElementsByClassName('btn__reset')[0];
 startButton.addEventListener('click', (e) => {
   overlay.style.display = 'none';
