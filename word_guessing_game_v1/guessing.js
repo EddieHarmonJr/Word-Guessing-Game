9fd3f6a73a72de1a566333efd83ee88b11d2607f
EddieHarmonJr <eddieharmon@ac.c4q.nyc>
2022-04-11 18:30:09 -0400

    create getRandomPhraseAsArray and addPhraseToDisplay functions

--- word_guessing_game_v1/guessing.js
+++ word_guessing_game_v1/guessing.js
@@ -1,7 +1,6 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const missed = 0;
-
 const overlay = document.getElementById('overlay');
 
 const phrases = [
@@ -12,14 +11,35 @@ const phrases = [
   "AT THE END OF THE DAY"
 ];
 
+//const test = ["i","t","o","l","d","y","o","u","s","o"];
+
 const startButton = document.getElementsByClassName('btn__reset')[0];
 startButton.addEventListener('click', (e) => {
   overlay.style.display = 'none';
 });
 
+
 const getRandomPhraseAsArray = (arr) => {
-  let randomArrayString = arr[Math.floor(Math.random() * arr.length)].split('');
-  return randomArrayString;
+  let randomSplitArray = arr[Math.floor(Math.random() * arr.length)].split('');
+  return randomSplitArray;
 };
 
+const phraseArray = getRandomPhraseAsArray(phrases);
+
+const addPhraseToDisplay = (arr) => {
+  for (var i = 0; i < arr.length; i++) {
+    const li = document.createElement('li');
+    const letter = arr[i];
+    li.textContent = letter;
+    phrase.appendChild(li);
+
+    if (arr[i]!== "") {
+      li.className = 'letter';
+    }
+  }
+
+};
+
+
 getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(phraseArray);
